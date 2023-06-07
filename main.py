45bf9de4c7ff208f4d5a8e06e47d54c4929644a7
LeroyAvietti <134562739+LeroyAvietti@users.noreply.github.com>
2023-06-07 23:57:41 +0000

    plural words
    added plural words and multiple questions

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ global items
 items = []
 global rounds
 rounds = 0
+questions = ["", "fruit", "food"]
 
 def game() :
   global rounds
@@ -17,23 +18,29 @@ def game() :
   global guesses
   global guess
   global answer
+  global question
   items = []
-  words = int(input('How many words do you want to play with?'))
-  for i in range(words):
-    heros = input("superhero ")
-    items.append(heros)
+  words = int(input('How many words do you want to play with? '))
+  question = random.choice(questions)
+  if words == 1 :
+    print(f"Name {words} {question}")
+  else :
+    print(f"Name {words} {question}s")
+  for i in range(words): 
+    inputs = input()
+    items.append(inputs)
   secret_word = random.choice(items)
   print(secret_word)
 
   rounds += 1
   print(f"Round {rounds}")
   print(items)
-  guess = input("guess which super hero is the secret word")
+  guess = input(f"guess which {question} is the secret word ")
   while guess != secret_word:
     print("nup")
     guesses -= 1
     if guesses != 0:
-      guess = input("guess which super hero is the secret word")
+      guess = input(f"guess which {question} is the secret word ")
     else:
       print("you lose")
       answer = input("play again? y/n")
