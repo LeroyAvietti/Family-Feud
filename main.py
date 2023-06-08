60b910ac3b914a745516c5aa5197c5d13714e94a
LeroyAvietti <134562739+LeroyAvietti@users.noreply.github.com>
2023-06-08 00:32:40 +0000

    final

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-#Leroy & Ethan's CATj
+#Leroy & Ethan's CAT
+
+#the variables and lists
 import random 
 global answer
 answer = 0
@@ -13,13 +15,15 @@ rounds = 0
 questions = ["", "fruit", "food"]
 
 def game() :
+  #an extra version of the global declorations to make sure its all working and setting down the items list and emptying it
   global rounds
   global items
   global guesses
   global guess
   global answer
-  global question
+  global questions
   items = []
+  #setting the list size, words in the list, and the secret word picked from it along with the question asked
   words = int(input('How many words do you want to play with? '))
   question = random.choice(questions)
   if words == 1 :
@@ -32,6 +36,7 @@ def game() :
   secret_word = random.choice(items)
   print(secret_word)
 
+  #round one begins, this begins the guessing segment, then the reveal of the secret wird and if you have one or lost. Then optionally beginning the game again if selected to do so
   rounds += 1
   print(f"Round {rounds}")
   print(items)
@@ -43,6 +48,7 @@ def game() :
       guess = input(f"guess which {question} is the secret word ")
     else:
       print("you lose")
+      print(f"The secret word was {secret_word}")
       answer = input("play again? y/n")
   print("yep")
   answer = input("play again? y/n")
